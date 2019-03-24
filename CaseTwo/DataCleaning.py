6a664ba7992489ef4be9c5c567c9e6f9a2b9c7a8
KimYiuLui <kimyiu-lui@hotmail.com>
2019-03-24 14:35:24 +0100

    add structure to folders

--- CaseTwo/DataCleaning.py
+++ CaseTwo/DataCleaning.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 inv_col = ['ProductId']
-inventory = pd.read_csv("./DataSets/inventory.csv", sep=";", names=inv_col, usecols=[0])
-employeeWitem = pd.read_csv	("./DataSets/peoplewithitems.csv", sep=";", usecols=[0,3])
+inventory = pd.read_csv("../DataSets/inventory.csv", sep=";", names=inv_col, usecols=[0])
+employeeWitem = pd.read_csv	("../DataSets/peoplewithitems.csv", sep=";", usecols=[0,3])
 
 df = employeeWitem.groupby(employeeWitem.columns.tolist(), as_index=False).size()
 print(df)
