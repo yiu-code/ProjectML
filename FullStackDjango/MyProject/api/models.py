09a0abf4af1ba1e9bae0519e4d0539aff3ec1489
Hayder1999 <hayderali1999@gmail.com>
2019-05-22 14:55:38 +0200

    blabla

--- FullStackDjango/MyProject/api/models.py
+++ FullStackDjango/MyProject/api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
+
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=255, unique=True)
+    
