f81f772015ab134e4a2e5152ea7cda6509c2645a
ImreBaas <imspons@gmail.com>
2019-05-23 11:53:57 +0200

    Basis van een API
    -Moet nog veranderd worden naar iets waar wij wat aan hebben.
    
    -Heeft ook een serialiser!

--- FullStackDjango/MyProject/api/admin.py
+++ FullStackDjango/MyProject/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Article, Author
 # Register your models here.
+#Maakt het mogelijk om bij het admin paneel deze dingen te wijzigen.
+admin.site.register(Article)
+admin.site.register(Author)
