907be5f7f33dbfa25f75f5cf269b8c578016f82e
Ibrahim <0944076@hr.nl>
2019-05-27 11:37:57 +0200

    web paginas toegevoegd
    Home page toegevoegd en de css toegevoegd. deze is werkend. Ook de juiste route naar de product page

--- FullStackDjango/api/urls.py
+++ FullStackDjango/api/urls.py
@@ -8,7 +8,7 @@ app_name = "articles"
 urlpatterns =  [
     path('home', views.Home, name="home-page"),
     path('register/', views.Register, name="registration-page"),
-    path('dbData', views.dbData, name='dbData'),
+    path('products', views.dbData, name='products'),
     path('', LoginView.as_view(template_name='login.html'), name="login"),
     #Ik wees eerlijk, ik heb niet echt een idee
     path('articles/<int:pk>', ArticleView.as_view()),
